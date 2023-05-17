176ea1c89b33351ade0172c0b5590e676bd9bfd8
Pavel <pates1993@mail.ru>
2023-05-17 23:32:52 +0300

    toolkit

--- src/components/VacansiesItem/Title/TitleVacansies.tsx
+++ src/components/VacansiesItem/Title/TitleVacansies.tsx
@@ -1,29 +1,11 @@
 import { FC } from 'react'
-import { useNavigate } from 'react-router-dom'
-
 
 interface PropsType {
-  id: number
-  profession: string
-  isCurrentVacancy: boolean
+	id: number
+	profession: string
+	isCurrentVacancy: boolean
 }
 
-export const TitleVacancy: FC<PropsType> = ({ id, profession, isCurrentVacancy }) => {
-  const navigate = useNavigate()
-
-  const navigateToVacancy = () => {
-    navigate(`${}${id}`)
-  }
-
-  return (
-    <div
-      onClick={navigateToVacancy}
-      className={classNames(styles.title, {
-        [styles.titleCurrent]: isCurrentVacancy,
-        [styles.disabled]: isLoading
-      })}
-    >
-      {profession}
-    </div>
-  )
+export const TitleVacancy: FC<PropsType> = () => {
+	return <div></div>
 }
